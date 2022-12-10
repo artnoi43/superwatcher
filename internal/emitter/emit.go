bd511e2563f7746cc12f75584dba3d9c397c7838
prem <pphansuriyanon@gmail.com>
2022-12-11 03:32:47 +0700

    refactor(emitter): add checks before performing expensive debugging

--- internal/emitter/emit.go
+++ internal/emitter/emit.go
@@ -10,25 +10,27 @@ import (
 func (e *emitter) emitFilterResult(result *superwatcher.FilterResult) {
 	if result != nil {
 		// Only log if there's some logs
-		var nilChan bool
+		nilChan := e.filterResultChan == nil
 		if len(result.GoodBlocks)+len(result.ReorgedBlocks) != 0 {
-			var goodBlocks []uint64
-			for _, b := range result.GoodBlocks {
-				goodBlocks = append(goodBlocks, b.Number)
-			}
+			// Use `if e.debug` to avoid expensive appends if emitter is not in debug mode
+			if e.debug {
+				var goodBlocks []uint64
+				for _, b := range result.GoodBlocks {
+					goodBlocks = append(goodBlocks, b.Number)
+				}
 
-			var reorgedBlocks []uint64
-			for _, b := range result.ReorgedBlocks {
-				reorgedBlocks = append(reorgedBlocks, b.Number)
-			}
+				var reorgedBlocks []uint64
+				for _, b := range result.ReorgedBlocks {
+					reorgedBlocks = append(reorgedBlocks, b.Number)
+				}
 
-			nilChan = e.filterResultChan == nil
-			e.debugger.Debug(
-				2, "emitFilterResult",
-				zap.Uint64s("goodBlocks", goodBlocks),
-				zap.Uint64s("reorgedBlocks", reorgedBlocks),
-				zap.Bool("nil resultChan", nilChan),
-			)
+				e.debugger.Debug(
+					2, "emitFilterResult",
+					zap.Uint64s("goodBlocks", goodBlocks),
+					zap.Uint64s("reorgedBlocks", reorgedBlocks),
+					zap.Bool("nil resultChan", nilChan),
+				)
+			}
 		}
 
 		if !nilChan {
