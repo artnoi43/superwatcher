ec43e4958a6cd991817de6bff6beba34f9a65b2b
prem <pphansuriyanon@gmail.com>
2022-12-11 03:08:10 +0700

    fix(convention): import name

--- superwatcher-demo/config/config.go
+++ superwatcher-demo/config/config.go
@@ -1,11 +1,11 @@
 package config
 
 import (
-	superwatcherConfig "github.com/artnoi43/superwatcher/config"
+	spwconf "github.com/artnoi43/superwatcher/config"
 )
 
 type Config struct {
-	SuperWatcherConfig *superwatcherConfig.EmitterConfig `mapstructure:"superwatcher_config" yaml:"superwatcher_config" json:"superwatcherConfig"`
-	RedisConnAddr      string                            `mapstructure:"redis_conn_addr" yaml:"redis_conn_addr" json:"redisConnAddr"`
-	Chain              string                            `mapstructure:"chain" yaml:"chain" json:"chain"`
+	SuperWatcherConfig *spwconf.EmitterConfig `mapstructure:"superwatcher_config" yaml:"superwatcher_config" json:"superwatcherConfig"`
+	RedisConnAddr      string                 `mapstructure:"redis_conn_addr" yaml:"redis_conn_addr" json:"redisConnAddr"`
+	Chain              string                 `mapstructure:"chain" yaml:"chain" json:"chain"`
 }
