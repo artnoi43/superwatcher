a54d76d4ccf121428bbbf430623dac293d3619dd
artworkk <prem@cleverse.com>
2022-12-18 23:33:23 +0700

    refactor(reorgsim): refactor global ReorgSimV2 tests, add test ReorgSimV2 cases

--- pkg/reorgsim/reorgsimv2_ethclient_impl_test.go
+++ pkg/reorgsim/reorgsimv2_ethclient_impl_test.go
@@ -7,21 +7,20 @@ import (
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/pkg/errors"
 )
 
 func TestFilterLogsV2(t *testing.T) {
-	param := ParamV1{
-		BaseParam: BaseParam{
-			StartBlock:    defaultStartBlock,
-			BlockProgress: 20,
-		},
-		ReorgEvent: ReorgEvent{
-			ReorgBlock: defaultReorgedAt,
-			MovedLogs:  nil,
-		},
+	param := BaseParam{
+		StartBlock:    defaultStartBlock,
+		BlockProgress: 20,
+	}
+	event := ReorgEvent{
+		ReorgBlock: defaultReorgedAt,
+		MovedLogs:  nil,
 	}
 
-	sim, err := NewReorgSimV2FromLogsFiles(param.BaseParam, []ReorgEvent{param.ReorgEvent}, defaultLogsFiles, 4)
+	sim, err := NewReorgSimV2FromLogsFiles(param, []ReorgEvent{event}, defaultLogsFiles, 4)
 	if err != nil {
 		t.Fatal("error creating ReorgSimV2", err.Error())
 	}
@@ -53,34 +52,22 @@ func TestFilterLogsV2(t *testing.T) {
 // Test if FilterLogs returns logs with correct hash (reorged hash).
 // No logs are moved in this test (yet).
 func TestFilterLogsReorgV2(t *testing.T) {
-	logsPath := "../../internal/emitter/assets"
-	logsFiles := []string{
-		logsPath + "/logs_lp.json",
-		logsPath + "/logs_poolfactory.json",
+	for i, test := range testsReorgSimV2 {
+		testFilterLogsReorgV2(t, i+1, test)
 	}
-	param := BaseParam{
-		StartBlock:    15944410,
-		ExitBlock:     15944530,
-		BlockProgress: 20,
-	}
-	event := ReorgEvent{
-		ReorgBlock: 15944415,
-		MovedLogs:  nil,
+}
+
+func testFilterLogsReorgV2(t *testing.T, testCase int, testConf multiReorgConfig) error {
+	if len(testConf.Events) == 0 {
+		return errors.New("got 0 ReorgEvent")
 	}
 
-	sim, err := NewReorgSimV2FromLogsFiles(
-		param,
-		[]ReorgEvent{event},
-		logsFiles,
-		2,
-	)
-
+	sim, err := NewReorgSimV2FromLogsFiles(testConf.Param, testConf.Events, testConf.LogsFiles, 2)
 	if err != nil {
-		t.Fatal("failed to create new ReorgSimV2", err.Error())
+		t.Fatalf("[%d] failed to create new ReorgSimV2: %s", testCase, err.Error())
 	}
 
 	rSim := sim.(*ReorgSimV2)
-
 	filter := func(base uint64) ([]types.Log, error) {
 		return rSim.FilterLogs(nil, ethereum.FilterQuery{
 			FromBlock: big.NewInt(int64(base) - 2),
@@ -88,28 +75,52 @@ func TestFilterLogsReorgV2(t *testing.T) {
 		})
 	}
 
-	for number := param.StartBlock; number < param.ExitBlock; number++ {
+	for number := testConf.Param.StartBlock; number < testConf.Param.ExitBlock; number++ {
 		logs, err := filter(number)
 		if err != nil {
-			t.Fatal("error from FilterLogs", err.Error())
+			return errors.Wrapf(err, "[%d] error from FilterLogs", testCase)
 		}
 		rLogs, err := filter(number)
 		if err != nil {
-			t.Fatal("error from FilterLogs", err.Error())
+			return errors.Wrapf(err, "[%d] error from FilterLogs", testCase)
 		}
 
 		for i, log := range logs {
-			if log.BlockNumber >= event.ReorgBlock {
-				rLog := rLogs[i]
+			if log.BlockNumber >= testConf.Events[len(testConf.Events)-1].ReorgBlock {
+				var rLog *types.Log
+
+				// TODO: How to test if lengths not identical?
+				if len(rLogs) == len(logs) {
+					rLog = &rLogs[i]
+				} else {
+					t.Log(testCase, log.BlockNumber, len(logs), len(rLogs))
+					for _, _rLog := range rLogs {
+						if _rLog.TxHash == log.TxHash && _rLog.TxIndex == log.TxIndex {
+							rLog = &_rLog
+							break
+						}
+					}
+				}
+
+				// If the reorgedLog is missing
+				if rLog == nil {
+					t.Fatalf(
+						"[%d] reorgedLog missing for log (block %d %s) (tx %s index %d)",
+						testCase, log.BlockNumber, log.BlockHash.String(), log.TxHash.String(), log.TxIndex,
+					)
+				}
 
 				if log.BlockHash == rLog.BlockHash {
 					// If hashes are the same, but it's reorged hash, then it's ok
 					if log.BlockHash == PRandomHash(log.BlockNumber) {
 						continue
 					}
+
 					t.Fatalf("[number = %d] log and rLog hashes match on %d-%d", number, log.BlockNumber, rLog.BlockNumber)
 				}
 			}
 		}
 	}
+
+	return nil
 }
