b9ac12396d596d8f942ec4c42eee670e29763079
artnoi43 <artdrawin@gmail.com>
2023-01-07 03:56:34 +0700

    refactor(poller): replace a for loop, and update comments

--- internal/poller/map_logs.go
+++ internal/poller/map_logs.go
@@ -17,10 +17,13 @@ type mapLogsResult struct {
 	superwatcher.Block
 }
 
-// mapLogs compare |logs| and their blockHashes to known blockHashes in |tracker.
-// We avoid getting block headers for all the range fromBlock-toBlock, so we use block hashes from the logs.
-// In case the known logs were removed from a particular block, then we won't have the fresh hash for that block.
-// In that case, we'll use |getHeaderFunc| to get the header for that particular block.
+// mapLogs maps logs to mapLogsResult, and compares |logs| and their blockHashes to
+// known blockHashes in |tracker|. If |tracker| is nil, mapLogs won't compare hashes.
+// If |doHeader| is true, mapLogs also gets all block headers for blocks with logs.
+// In case the known logs were removed from a particular block, then we won't have
+// the fresh hash/header for that block to compare to tracker values.
+// In that case, we'll use |client| to call getHeadersByNumbers to get the header for
+// blocks with missing logs, regardless of |doHeader| value, so that we have the new block hash.
 func mapLogs(
 	// For calling |getHeaderFunc|
 	ctx context.Context,
@@ -87,7 +90,7 @@ func mapLogs(
 	var headers map[uint64]superwatcher.BlockHeader
 	var blocksWithResults []uint64
 
-	// Get block headers using BatchCallContext
+	// Get block headers using BatchCallContext via getHeadersByNumbers
 	if doHeader {
 		for n := toBlock; n >= fromBlock; n-- {
 			if _, ok := mapResults[n]; !ok {
@@ -149,8 +152,36 @@ func mapLogs(
 		}
 
 		mapResult, ok := mapResults[n]
+		// If !ok, then it's one of blocksMissingLogs
 		if !ok {
-			continue
+			// Mark blocks with missing logs as reorged. Do not overwrite tracker data here,
+			// as poller.Poll logic needs the old info in the tracker to return as reorged block.
+			// If you update the tracker data now, downstream users won't get all the old data
+			// they need to handle chain reorg events.
+			newHeader, ok := headers[n]
+			if !ok {
+				return nil, errors.Wrapf(superwatcher.ErrProcessReorg, "block %d (missing logs) header not found", n)
+			}
+
+			trackerBlock, ok := tracker.getTrackerBlock(n)
+			if !ok {
+				return nil, errors.Wrapf(superwatcher.ErrProcessReorg, "block %d (missing logs) not found in tracker", n)
+			}
+
+			newHash := newHeader.Hash()
+			if newHash == trackerBlock.Hash {
+				continue
+			}
+
+			mapResult = &mapLogsResult{
+				reorged: true,
+				Block: superwatcher.Block{
+					Number: n,
+					Header: newHeader,
+					Hash:   newHash,
+					Logs:   nil,
+				},
+			}
 		}
 
 		if trackerBlock.Hash == mapResult.Block.Hash && len(trackerBlock.Logs) == len(mapResult.Block.Logs) {
@@ -163,36 +194,6 @@ func mapLogs(
 		}
 
 		mapResult.reorged = true
-	}
-
-	// Mark blocks with missing logs as reorged. Do not overwrite tracker data here,
-	// as poller.Poll logic needs the old info in the tracker to return as reorged block.
-	// If you update the tracker data now, downstream users won't get the old info they need to handle chain reorg.
-	for _, n := range blocksMissingLogs {
-		newHeader, ok := headers[n]
-		if !ok {
-			return nil, errors.Wrapf(superwatcher.ErrProcessReorg, "block %d (missing logs) header not found", n)
-		}
-
-		trackerBlock, ok := tracker.getTrackerBlock(n)
-		if !ok {
-			return nil, errors.Wrapf(superwatcher.ErrProcessReorg, "block %d (missing logs) not found in tracker", n)
-		}
-
-		newHash := newHeader.Hash()
-		if newHash == trackerBlock.Hash {
-			continue
-		}
-
-		mapResults[n] = &mapLogsResult{
-			reorged: true,
-			Block: superwatcher.Block{
-				Number: n,
-				Header: newHeader,
-				Hash:   newHash,
-				Logs:   nil,
-			},
-		}
 	}
 
 	return mapResults, nil
